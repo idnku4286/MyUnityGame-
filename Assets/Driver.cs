ba955b281c76cc6fa1d45f4dbeacc674030b94a3
Minwoo <idnku1107@gmail.com>
2025-08-06 00:26:46 +0900

    fix: Driver.cs 속도 조정 추가

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class Driver : MonoBehaviour
 {
     [SerializeField] float steerSpeed = 0.1f;
-    [SerializeField] float moveSpeed = 0.01f;   
+    [SerializeField] float moveSpeed = 20f;
+    [SerializeField] float slowSpeed = 15f;
+    [SerializeField] float fastSpeed = 30f;
     void Start()
     {
         
@@ -19,5 +21,22 @@ public class Driver : MonoBehaviour
         transform.Rotate(0, 0, -steerAmount);
         transform.Translate(0, moveAmount, 0);
 
+    }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+     
+        if (other.gameObject.tag == "Obstacle")
+        {
+            Debug.Log("�浹�߽��ϴ�.");
+            moveSpeed = slowSpeed;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Boost")
+        {
+            Debug.Log("�ν��͸� �޾ҽ��ϴ�.");
+            moveSpeed = fastSpeed;
+        }
     }
 }
